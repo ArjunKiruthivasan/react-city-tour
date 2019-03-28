538fe15e409c488d002dbd535bbfdadf90894c4f
ArjunKiruthivasan <arjun.kiruthivasan@gmail.com>
2019-03-28 15:07:59 +0530

    first commit

---
+++ src/components/tourData.js
@@ -0,0 +1,34 @@
+export const tourData = [
+  {
+    id: 1,
+    city: "Chennai",
+    img: "./img/chennai_central.jpeg",
+    name: "chennai central tour",
+    info:
+      "Chennai Central is the main railway terminus in the city of Chennai, Tamil Nadu, India. It is the busiest railway station in South India and one of the most important hubs in the country."
+  },
+  {
+    id: 2,
+    city: "Bengaluru",
+    img: "./img/Bangalore_city.jpeg",
+    name: "Bengaluru city",
+    info:
+      "Bangalore, officially known as Bengaluru (About this soundlisten)), is the capital city of the Indian state of Karnataka. It has a population of over ten million, making it a megacity and the third most populous city and fifth most populous urban agglomeration in India."
+  },
+  {
+    id: 3,
+    city: "Thiruvananthapuram",
+    img: "./img/tvm_temple2.jpg",
+    name: "Padmanabhaswamy Temple",
+    info:
+      "Padmanabhaswamy Temple is located in Thiruvananthapuram, Kerala, India. The temple is built in an intricate fusion of the indigenous Kerala style and the Tamil style (kovil) of architecture associated with the temples located in the neighbouring state of Tamil Nadu, featuring high walls, and a 16th-century Gopuram."
+  },
+  {
+    id: 4,
+    city: "Madurai",
+    img: "./img/madurai_temple.jpg",
+    name: "Meenakshi Amman Temple",
+    info:
+      "The Meenakshi temple complex at Madurai is a city temple. It has eminent and exquisitely carved towers enveloping the temple, dedicated to Goddess Meenakashi in Madurai. Considered as the south gateway, the Meenakshi temple consists of the twin temples of God Shiva and Goddess Meenakshi, each one as high as about nine storeys."
+  }
+];
