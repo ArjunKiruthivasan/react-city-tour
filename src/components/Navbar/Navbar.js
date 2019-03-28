3138d63ae6d415e72f232324d9bc6bc28c057d64
ArjunKiruthivasan <arjun.kiruthivasan@gmail.com>
2019-03-28 15:59:36 +0530

    header fixed

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -3,6 +3,7 @@ import "./Navbar.scss";
 import logo from "../../logo.png";
 
 export default function Navbar() {
+  
   return (
     <nav className="navbar">
       <img src={logo} alt="city tours logo" />
